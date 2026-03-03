8369911f5128f85cad9e340dd38076a6c63144f3
Karol Wojtyla <karolwojtyla360@gmail.com>
2026-03-03 17:18:29 -0300

    feat: add footer icons

---
+++ src/assets/icons/threads.tsx
@@ -0,0 +1,31 @@
+import { cn } from "@/lib/utils";
+
+interface ThreadsProps {
+  width?: number;
+  height?: number;
+  className?: string;
+}
+
+const Threads = ({ width, height, className }: ThreadsProps) => {
+  return (
+    <svg
+      className={cn("size-6", className)}
+      xmlns="http://www.w3.org/2000/svg"
+      width={width || "1em"}
+      height={height || "1em"}
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+        d="M19.25 8.505c-1.577-5.867-7-5.5-7-5.5s-7.5-.5-7.5 8.995s7.5 8.996 7.5 8.996s4.458.296 6.5-3.918c.667-1.858.5-5.573-6-5.573c0 0-3 0-3 2.5c0 .976 1 2 2.5 2s3.171-1.027 3.5-3c1-6-4.5-6.5-6-4"
+        color="currentColor"
+      ></path>
+    </svg>
+  );
+};
+
+export default Threads;
---
+++ src/assets/icons/x.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@/lib/utils";
+
+interface XProps {
+  width?: number;
+  height?: number;
+  className?: string;
+}
+
+const X = ({ width, height, className }: XProps) => {
+  return (
+    <svg
+      className={cn("size-6", className)}
+      xmlns="http://www.w3.org/2000/svg"
+      width={width || "1em"}
+      height={height || "1em"}
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="currentColor"
+        d="M10.488 14.651L15.25 21h7l-7.858-10.478L20.93 3h-2.65l-5.117 5.886L8.75 3h-7l7.51 10.015L2.32 21h2.65zM16.25 19L5.75 5h2l10.5 14z"
+      ></path>
+    </svg>
+  );
+};
+
+export default X;
